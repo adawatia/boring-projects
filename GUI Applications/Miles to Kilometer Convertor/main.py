3842a44065d4f2f8d3c0d260c926951618d91d13
Devansh Sharma <devsansh@yahoo.com>
2024-09-11 16:16:28 +0530

    Application Complete.

---
+++ GUI Applications/Miles to Kilometer Convertor/main.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import ttk
+
+def convert():
+    miles = user_input.get()
+    kilometers = str(round(miles * 1.61,2))
+    output_str.set(kilometers + "Km")
+    
+window = tk.Tk()
+window.title("MtK")
+window.geometry("320x150")
+
+# Title
+title_label = ttk.Label(master = window, text = "Miles to Kilomters", font = "Calibri 24 bold")
+title_label.pack()
+
+# User input field
+input_frame = ttk.Frame(master = window)
+
+user_input  = tk.IntVar()
+input_field = ttk.Entry(master = input_frame,textvariable = user_input)
+button = ttk.Button(master = input_frame, text = "Convert", command = convert)
+input_field.pack(side = 'left', padx = 12)
+button.pack(side = 'left')
+input_frame.pack(padx = 15)
+
+# Output
+output_str = tk.StringVar()
+output_label  = ttk.Label(master = window, font = "Calibri 24", textvariable = output_str)
+output_label.pack(pady = 10)
+
+# Run
+window.mainloop()
